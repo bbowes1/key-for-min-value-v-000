562b6f3c924627ddb6bf6e0a406c511f2b9ab4d6
Benjamin Bowes <bbowes999@gmail.com>
2018-03-28 11:31:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,4 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  hash.each do |(key, value) index|
-    if index < 2
-   index
-    else
-      nil
-end
- end
+
